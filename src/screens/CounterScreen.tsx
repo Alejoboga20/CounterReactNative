478b818adba65d6d655bb36519b992723aa82749
Alejo Boga <alejoboga20@gmail.com>
2022-03-12 08:44:41 -0300

    Adding FABs in counter

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -8,9 +8,19 @@ export const CounterScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Counter: {counter}</Text>
 
-      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
-        <View style={styles.buttonIncrement}>
-          <Text>+1</Text>
+      <TouchableOpacity
+        onPress={() => setCounter(counter + 1)}
+        style={styles.fabLocationBR}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => setCounter(counter - 1)}
+        style={styles.fabLocationBL}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -20,5 +30,27 @@ export const CounterScreen = () => {
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center' },
   title: { fontSize: 40, textAlign: 'center', top: -15 },
-  buttonIncrement: { backgroundColor: 'blue', borderRadius: 100 },
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856d6',
+    borderRadius: 100,
+    justifyContent: 'center',
+    height: 60,
+    width: 60,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
 });
