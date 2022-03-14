55d64c6af3b03d001b53eec8d8b1cc4f6151c4bc
Alejo Boga <alejoboga20@gmail.com>
2022-03-14 14:06:10 -0300

    Using flex direction

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -14,22 +14,21 @@ export const FlexScreen = () => {
 const styles = StyleSheet.create({
   contaier: {
     flex: 1,
+    flexDirection: 'row-reverse',
     backgroundColor: '#28c4d9',
   },
   box1: {
-    flex: 5,
+    flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box2: {
-    flex: 3,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box3: {
-    flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
